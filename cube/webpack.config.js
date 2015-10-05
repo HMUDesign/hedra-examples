b6bdbab818edaa98439d7c0ea9f5e31e9664167b
Christopher Baker <christopher@hmudesign.com>
2015-10-04 22:33:07 -0700

    add cube

---
+++ cube/webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+	devtool: 'source-map',
+	
+	entry: './src/bootstrap.js',
+	output: {
+		path: __dirname + '/build',
+		filename: '[name].bundle.js',
+	},
+	
+	module: {
+		loaders: [
+			{
+				test: /\.jsx$/,
+				loader: 'babel?optional[]=runtime',
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel?optional[]=runtime',
+				exclude: /(node_modules)/,
+			},
+			{
+				test: /\.glsl$/,
+				exclude: /(node_modules)/,
+				loader: 'hedra/loader-webpack-glsl',
+			},
+		],
+	},
+	
+	resolve: {
+		extensions: [ '', '.js', '.jsx' ],
+		modulesDirectories: [ 'vendor', 'node_modules' ],
+	},
+	
+	devServer: {
+		contentBase: './build',
+		stats: {
+			modules: false,
+			cached: false,
+			colors: true,
+			chunk: false,
+		},
+	},
+};
